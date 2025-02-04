28c64dd2e33ad536c5f8f305bda074c5d9c4d470
Upayan Mazumder <upayanm3@gmail.com>
2025-02-04 17:19:57 +0530

    Wrap homepage text in paragraph tags for improved semantics

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function Home() {
   return (
     <main>
-        This is the homepage
+        <p>This is the homepage</p>
         <Image src="/icons/icon-512x512.png" alt="PurByte Logo" width={512} height={512} />
     </main>
   );
